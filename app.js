b0274b1032f610397fb623722c5dc1392cb7b0d6
brunocesarangst <bruno.angst@rede.ulbra.br>
2021-07-29 01:21:17 -0300

    currency

--- app.js
+++ app.js
@@ -28,3 +28,111 @@
       - Para obter a key e fazer requests, você terá que fazer login e escolher
         o plano free. Seus dados de cartão de crédito não serão solicitados.
 */
+
+const currencyOneEl = document.querySelector('[data-js="currency-one"]');
+const currencyTwoEl = document.querySelector('[data-js="currency-two"]');
+
+const currenciesEl = document.querySelector('[data-js="currencies-container"]');
+
+const convertedValueEl = document.querySelector('[data-js="converted-value"]')
+
+const valuePrecisionEl = document.querySelector('[data-js="conversion-precision"]')
+
+const timesCurrencyEl = document.querySelector('[data-js="currency-one-times"]')
+
+let internalExchangeRate = {}
+
+const url =
+  "https://v6.exchangerate-api.com/v6/ae3951b7a5df6c73424f0705/latest/USD";
+
+const getErrorMessage = (errorType) =>
+  ({
+    "unsupported-code":
+      "Desculpa, essa moeda não existe em nosso banco de dados.",
+    "malformed-request":
+      "O endpoint dessa request não segue a estrutura padrão. Mais informações: https://www.exchangerate-api.com/docs/standard-requests",
+    "invalid-key": "Chave API inválida.",
+    "quota-reached": "A conta atingiu o limite.",
+    "not-available-on-plan": "Ação não permitida.",
+  }[errorType] || "Não foi possível obter as informações.");
+
+const fetchExchangeRate = async () => {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(
+        "Sua conexão falhou, nào foi possível obter as informações."
+      );
+    }
+
+    // console.log(await response.json())
+    const exchangeRateData = await response.json();
+
+    if (exchangeRateData.result === "error") {
+      throw new Error(getErrorMessage(exchangeRateData["error-type"]));
+    }
+
+    return exchangeRateData;
+  } catch (error) {
+    const div = document.createElement("div");
+    const button = document.createElement("button");
+
+    div.textContent = error.message;
+
+    div.classList.add(
+      "alert",
+      "alert-warning",
+      "alert-dismissible",
+      "fade",
+      "show"
+    );
+    div.setAttribute("role", "alert");
+
+    button.classList.add("btn-close");
+    button.setAttribute("type", "button");
+    button.setAttribute("arial-label", "close");
+
+    button.addEventListener("click", () => {
+      div.remove();
+    });
+
+    div.appendChild(button);
+    currenciesEl.insertAdjacentElement("afterend", div);
+  }
+};
+
+const init = async () => {
+  // console.log(await fetchExchangeRate());
+
+  const exchangeRateData = await fetchExchangeRate()
+
+  internalExchangeRate = { ...exchangeRateData }
+
+  const getOptions = selectedCurrency => Object.keys(exchangeRateData.conversion_rates)
+    .map(currency => `<option ${currency === selectedCurrency ? 'selected' : ''}>${currency}</option>`)
+    .join('');
+
+    console.log(getOptions)
+  
+
+  currencyOneEl.innerHTML = getOptions('USD');
+  currencyTwoEl.innerHTML = getOptions('BRL');
+
+  convertedValueEl.textContent = exchangeRateData.conversion_rates.BRL.toFixed(2)
+
+  valuePrecisionEl.textContent = `1 USD = ${exchangeRateData.conversion_rates.BRL} BRL`
+
+};
+
+timesCurrencyEl.addEventListener('input', e => {
+  convertedValueEl.textContent = (e.target.value * internalExchangeRate.conversion_rates[currencyTwoEl.value]).toFixed(2)
+})
+
+currencyTwoEl.addEventListener('input', e => {
+  const currencyTwoValue = internalExchangeRate.conversion_rates[e.target.value]
+
+  convertedValueEl.textContent = (timesCurrencyEl.value * currencyTwoEl).toFixed(2)
+})
+
+init();
